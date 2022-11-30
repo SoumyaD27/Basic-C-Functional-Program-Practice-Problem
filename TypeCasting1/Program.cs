b6f1b58e001ff1124eb57641b9cda07da9a9f51f
SoumyaD27 <soumyad233@gmail.com>
2022-11-30 18:31:36 +0530

    [Soumya]-Type casting

---
+++ TypeCasting1/Program.cs
@@ -0,0 +1,30 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace TypeCasting
+{
+    internal class Program
+    {
+        static void Main(string[] agrs)
+        {
+            int a = 20;
+            float b = a;
+            Console.WriteLine(b);                      //Implicit type casting
+            float c = 20.34f;
+            int d = (int)c;                            //Explicit type casting
+            Console.WriteLine(d);
+
+            Console.WriteLine("Enter 1st number: ");
+            int v = int.Parse(Console.ReadLine());     //Type Conversion using Parse()
+            Console.WriteLine(v);
+
+            Console.WriteLine("Enter number:");
+            double f = Convert.ToDouble(Console.ReadLine());  //Type Conversion using Convert Class
+            Console.WriteLine(f);
+
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();          //Read the string values
+            Console.WriteLine(name);
+        }
+    }
+}
