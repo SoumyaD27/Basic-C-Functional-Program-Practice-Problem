e94c997855adb64e2cf3563814f62c527dac20bc
SoumyaD27 <soumyad233@gmail.com>
2022-11-30 21:06:42 +0530

    [Soumya]-Even or Odd Number

---
+++ EvenOrOddNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace EvenOrOddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number: ");
+            int n = int.Parse(Console.ReadLine());
+            if(n%2== 0)
+            {
+                Console.WriteLine("It's a Even Number.");
+            }
+            else
+            {
+                Console.WriteLine("It's a Odd Number.");
+            }
+        }
+    }
+}
