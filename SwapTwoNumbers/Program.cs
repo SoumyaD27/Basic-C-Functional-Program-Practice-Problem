afcf75d11524ba385eb37141190e98f837662c48
SoumyaD27 <soumyad233@gmail.com>
2022-11-30 20:57:01 +0530

    [Soumya]-Swapping Two Numbers

---
+++ SwapTwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace SwapTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+            Console.WriteLine("Enter first number: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("After swapping {0},{1}", num1, num2);
+        }
+    }
+}
