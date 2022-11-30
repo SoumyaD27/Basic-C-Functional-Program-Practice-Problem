25dc875261c150ea6f11d5f269e15f5d803e4e37
SoumyaD27 <soumyad233@gmail.com>
2022-11-30 20:34:01 +0530

    [Soumya]-Harmonic Number Series

---
+++ HarmonicNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace HarmonicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            Console.WriteLine("Number of terms:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i=1;i<=n;i++)
+            {
+                Console.Write("1/{0}+", i);
+            }
+        }
+    }
+}
