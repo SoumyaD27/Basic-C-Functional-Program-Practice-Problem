0fac26397998f7316772d5a09f8d2d2294b957c8
SoumyaD27 <soumyad233@gmail.com>
2022-11-30 19:44:07 +0530

    [Soumya]-Leap Year

---
+++ LeapYear/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter the year: ");
+            year = Convert.ToInt32(Console.ReadLine());
+            if(year % 4 == 0)
+            {
+                Console.WriteLine("This year is leap year");
+            }
+            else
+            {
+                Console.WriteLine("This year is not leap year");
+            }
+            Console.ReadLine();
+        }
+    }
+}
