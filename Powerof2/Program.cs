7e143cd3f53bc4a768a042d647b1290c4ea7e45b
SoumyaD27 <soumyad233@gmail.com>
2022-11-30 20:11:20 +0530

    [Soumya]-Power of 2

---
+++ Powerof2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Powerof2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Give the Exponent");
+            n = Convert.ToInt32(Console.ReadLine());
+            double value = Math.Pow(2, n);
+            Console.WriteLine("Result: {0}", value);
+            Console.ReadLine();
+        }
+    }
+}
