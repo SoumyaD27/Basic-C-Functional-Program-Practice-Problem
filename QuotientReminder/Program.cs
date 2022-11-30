a3ab04dfa8d9d75c7ae5b6437591e77588459c36
SoumyaD27 <soumyad233@gmail.com>
2022-11-30 20:46:19 +0530

    [Soumya]-Compute Quotient and Reminder

---
+++ QuotientReminder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace QuotientReminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b: ");
+            b = int.Parse(Console.ReadLine());
+            int quot = a / b;
+            int remi = a % b;
+            Console.WriteLine("{0} is quotient,{1} is reminder",quot, remi);
+
+
+        }
+    }
+}
