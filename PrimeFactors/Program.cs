c4462027e505b5ddabc80845ac3955e6f983c82e
SoumyaD27 <soumyad233@gmail.com>
2022-11-30 20:24:22 +0530

    [Soumya]-Prime Factors

---
+++ PrimeFactors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace PrimeFactors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.WriteLine("Enter the Number: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Factors: ");
+            for (i=1;i<=n;i++)
+            {
+                if(n%i==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
